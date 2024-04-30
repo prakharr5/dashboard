3e308bb7078319331ba0acdd8b592fd271fa39cf
prakharr5 <prakharkumar.srivastava.22cse@bmu.edu.in>
2024-04-30 15:34:23 +0530

    structure

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,58 @@ import "./Hero.css";
 function Hero() {
   return (
     <>
-      <main>Hero</main>
+      <main>
+        <div className="left">
+          <p>Dashboards</p>
+          <div className="boxs">
+            <div className="box">
+              <i className="fa-regular fa-circle-user"></i>
+              Patients
+            </div>
+            <div className="line"></div>
+            <div className="box hr">
+              <i className="fa-solid fa-coins"></i>HR
+            </div>
+            <div className="line"></div>
+            <div className="box">
+              <i className="fa-solid fa-filter"></i>Labs
+            </div>
+            <div className="line"></div>
+            <div className="box">
+              <i className="fa-solid fa-house-chimney-medical"></i>Pharma
+            </div>
+          </div>
+          <p>Processes</p>
+          <div className="boxs">
+            <div className="box">
+              <i className="fa-solid fa-newspaper"></i>Registration
+            </div>
+            <div className="line"></div>
+            <div className="box">
+              <i className="fa-regular fa-file-lines"></i>Consultation
+            </div>
+            <div className="line"></div>
+            <div className="box">
+              <i className="fa-solid fa-vial"></i>Tests & Reports
+            </div>
+            <div className="line"></div>
+            <div className="box">
+              <i className="fa-solid fa-money-bills"></i>Billing
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div className="consul">Consultants</div>
+          <div className="filt">f</div>
+          <div className="count">
+            <div className="boxTwo">Consultant Count</div>
+            <div className="boxTwo">Consultations</div>
+            <div className="boxTwo">Labs</div>
+            <div className="boxTwo">Diet Plans</div>
+          </div>
+          <div className="tabl">Consultants</div>
+        </div>
+      </main>
     </>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,21 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <>
-      <div className="Navbar">Navbar</div>
+      <div className="Navbar">
+        <div className="middle">
+          <div className="search">
+            <input type="dropdown" placeholder="Patients" />
+            <input type="search" placeholder="Search" />
+          </div>
+          <div className="addNew">
+            <button>Add New</button>
+          </div>
+        </div>
+        <div className="profile">
+          <div className="info"></div>
+          <div className="acc"></div>
+        </div>
+      </div>
     </>
   );
 }
