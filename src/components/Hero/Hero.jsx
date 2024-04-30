f51397a8548dcb67563733e027ebda5213f5b8ff
prakharr5 <prakharkumar.srivastava.22cse@bmu.edu.in>
2024-04-30 16:10:56 +0530

    structure

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -45,15 +45,99 @@ function Hero() {
           </div>
         </div>
         <div className="right">
-          <div className="consul">Consultants</div>
-          <div className="filt">f</div>
-          <div className="count">
-            <div className="boxTwo">Consultant Count</div>
-            <div className="boxTwo">Consultations</div>
-            <div className="boxTwo">Labs</div>
-            <div className="boxTwo">Diet Plans</div>
+          <div className="consul">
+            <i className="fa-solid fa-users"></i>Consultants
           </div>
-          <div className="tabl">Consultants</div>
+          <div className="filt">
+            Filters
+            <input type="dropdown" placeholder="Select Range" />
+            <input type="radio" id="sum" />
+            <label htmlFor="sum">Summary</label>
+            <input type="radio" id="branch" />
+            <label htmlFor="branch">Branch Wise</label>
+          </div>
+          <div className="count">
+            <div className="boxTwo">
+              <div className="content">
+                Consultant Count <br /> <b>90/900</b> <br /> Today/Period
+              </div>
+              <i className="fa-solid fa-users"></i>
+            </div>
+            <div className="boxTwo">
+              <div className="content">
+                Consultations
+                <br /> <b>11/25</b> <br /> Today/Period
+              </div>
+              <i className="fa-solid fa-file-lines"></i>
+            </div>
+            <div className="boxTwo">
+              <div className="content">
+                Labs
+                <br /> <b>25/560</b> <br /> Today/Period
+              </div>
+              <i className="fa-solid fa-money-bill"></i>
+            </div>
+            <div className="boxTwo">
+              <div className="content">
+                Diet Plan
+                <br /> <b>25/250</b> <br /> Today/Period
+              </div>
+              <i className="fa-solid fa-money-bill"></i>
+            </div>
+          </div>
+          <div className="tabl">
+            <div className="head">
+              Consultants
+              <div className="back">
+                <i className="fa-solid fa-arrow-rotate-right"></i>
+                <i className="fa-solid fa-arrow-down"></i>
+              </div>
+            </div>
+            <table>
+              <tr>
+                <th>NAME</th>
+                <th>PATIENT COUNT</th>
+                <th>LABS</th>
+                <th>DIET PLANS</th>
+              </tr>
+              <tr>
+                <td>John Smith</td>
+                <td>12</td>
+                <td>3</td>
+                <td>6</td>
+              </tr>
+              <tr>
+                <td>John Smith</td>
+                <td>12</td>
+                <td>3</td>
+                <td>6</td>
+              </tr>
+              <tr>
+                <td>John Smith</td>
+                <td>12</td>
+                <td>3</td>
+                <td>6</td>
+              </tr>
+              <tr>
+                <td>John Smith</td>
+                <td>12</td>
+                <td>3</td>
+                <td>6</td>
+              </tr>
+              <tr>
+                <td>John Smith</td>
+                <td>12</td>
+                <td>3</td>
+                <td>6</td>
+              </tr>
+              <tr>
+                <td>John Smith</td>
+                <td>12</td>
+                <td>3</td>
+                <td>6</td>
+              </tr>
+            </table>
+          </div>
         </div>
       </main>
     </>
